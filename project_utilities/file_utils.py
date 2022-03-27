81547a8df5d76e9ed2ff882d36d5cc030e7b6263
RickarySanchez <horgan098@gmail.com>
2022-03-27 22:00:23 +0100

    minor improvements and small bug fix

--- project_utilities/file_utils.py
+++ project_utilities/file_utils.py
@@ -2,17 +2,15 @@ import glob
 import os
 import sys 
 
-def find_files(directory:str, file:str, exit:bool()=False, recursive:bool()=True):
 
+def find_files(directory: str, file: str, exit: bool() = False, recursive: bool() = True):
     error_message = "ERROR " + file + " not found !"
     files = []
 
     if recursive:
-
-        files.append(glob.glob(os.path.join(directory, '**', file), recursive=recursive))
-
-    else: 
-        files.append(glob.glob(os.path.join(directory, file), recursive=recursive))
+        files += glob.glob(os.path.join(directory, '**', file), recursive=recursive)
+    else:
+        files += glob.glob(os.path.join(directory, file), recursive=recursive)
 
     if len(files) == 0:
         if exit:
@@ -26,5 +24,6 @@ def find_files(directory:str, file:str, exit:bool()=False, recursive:bool()=True
     else: 
         return files
 
-if __name__ == 'main':
-    print(find_files("C:/Users/horga/project-utilities", "LICENSE"))
+
+if __name__ == '__main__':
+    print(find_files("C:/Users/horga", ".gitconfig", exit=True))
